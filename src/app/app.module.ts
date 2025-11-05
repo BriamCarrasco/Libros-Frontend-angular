0950c8eb433e6cfb21b3f84cfdc4528064bcd1af
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-05 00:39:13 -0300

    Se crean componentes para aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdminLibrosComponent } from './admin-libros/admin-libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    FooterComponent,
+    AdminLibrosComponent
+    
   ],
   imports: [
     BrowserModule,
