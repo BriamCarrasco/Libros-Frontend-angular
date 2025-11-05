455d8d5d75563889c5f83f5e72c787e24854bb6c
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-05 04:24:53 -0300

    Se agregan componentes e imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopbarComponent } from './topbar/topbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { AdminLibrosComponent } from './admin-libros/admin-libros.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent,
     FooterComponent,
     AdminLibrosComponent
-    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TopbarComponent,
+    HttpClientModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
