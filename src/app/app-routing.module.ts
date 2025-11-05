bdefd431d145af670cd447d4d7a8a118e6c8ade3
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-05 04:26:32 -0300

    Se agregan componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLibrosComponent } from './admin-libros/admin-libros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AdminLibrosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
